55420b18840f777e6972c564106fb23c6da74745
Matt <matt.cadart@gmail.com>
2017-05-09 21:30:54 +0200

    add multiple numbers

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,12 +1,20 @@
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 
     public int add(String numbers){
-        if(numbers.equals(""))
-            return (Integer) 0;
-        else
-            return Integer.parseInt(numbers);
+        List<String> numList = Arrays.asList(numbers.split(","));
+
+        if (numList.size() == 1) {
+            if (numbers.equals(""))
+                return 0;
+            else
+                return Integer.parseInt(numbers);
+        } else {
+            return numList.stream().map(Integer::parseInt).collect(Collectors.toList()).stream().mapToInt(Integer::intValue).sum();
+        }
     }
 }
 
