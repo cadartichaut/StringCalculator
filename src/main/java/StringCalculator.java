fb1f4fbe20217b259838914004f8c07b557b5578
Matt <matt.cadart@gmail.com>
2017-05-10 23:31:56 +0200

    avancement jusqu'a la fin du beginner

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -6,9 +7,21 @@ import java.util.stream.Collectors;
 public class StringCalculator {
 
     public int add(String numbers){
-        List<String> numList = Arrays.asList(numbers.split(","));
+        List<String> numList = Arrays.asList(numbers.split("[^0-9-]+"));
 
         Collections.replaceAll(numList, "", "0");
+
+        List<String> negatives = new ArrayList<>();
+
+        numList.forEach((number) -> {
+            if (Integer.parseInt(number) < 0) {
+                negatives.add(number);
+            }
+        });
+
+        if (negatives.size() > 0)
+            throw new RuntimeException("Negatives not allowed : " + negatives.stream().collect(Collectors.joining(",")));
+
 
         return numList.stream().map(Integer::parseInt).collect(Collectors.toList()).stream().mapToInt(Integer::intValue).sum();
     }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,6 +1,9 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class StringCalculatorTest {
     private StringCalculator stringCalculator = new StringCalculator();
 
@@ -25,4 +28,34 @@ public class StringCalculatorTest {
         Assert.assertEquals(3, stringCalculator.add(",3"));
     }
 
+    @Test
+    public void addTenNumbersShouldReturnTheCorrectSum() {
+        Assert.assertEquals(51, stringCalculator.add("2,4,6,8,10,10,10,1"));
+    }
+
+    @Test
+    public void allowLineReturnAsSeparator() {
+        Assert.assertEquals(10, stringCalculator.add("5\n5"));
+    }
+
+    @Test
+    public void allowCustomDelimiter() {
+        Assert.assertEquals(10, stringCalculator.add("//;6;4"));
+    }
+
+    @Test
+    public void allowEverything() {
+        Assert.assertEquals(12, stringCalculator.add("as2>1!2;5'2"));
+    }
+
+    @Test
+    public void negativeNumbersShouldThrowException() {
+        try {
+            stringCalculator.add("-1, 3");
+            fail("Expected a RuntimeException to be thrown");
+        } catch (RuntimeException ex) {
+            assertEquals(ex.getMessage(), "Negatives not allowed : -1");
+        }
+
+    }
 }
